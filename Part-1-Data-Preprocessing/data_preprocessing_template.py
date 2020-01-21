8b512edb8ea6937f76f68be077c0a4d8d0262640
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-21 16:31:00 +0600

    Importing the dataset

--- Part-1-Data-Preprocessing/data_preprocessing_template.py
+++ Part-1-Data-Preprocessing/data_preprocessing_template.py
@@ -4,3 +4,8 @@
 import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
