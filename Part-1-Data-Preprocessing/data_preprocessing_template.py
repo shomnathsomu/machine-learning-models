69268bd9983bb39a1126636bf9f491047866f92c
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-21 16:31:55 +0600

    Taking care of missing data

--- Part-1-Data-Preprocessing/data_preprocessing_template.py
+++ Part-1-Data-Preprocessing/data_preprocessing_template.py
@@ -9,3 +9,9 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer 
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean', verbose=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
