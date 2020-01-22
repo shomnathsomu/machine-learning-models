455b1aed1514668b82c2a8b6b9437d7ebd7b6d98
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-22 16:39:21 +0600

    Predicting the Test set results

--- Part-2-Regression/Simple-Linear-Regression/simple_linear_regression.py
+++ Part-2-Regression/Simple-Linear-Regression/simple_linear_regression.py
@@ -18,3 +18,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
