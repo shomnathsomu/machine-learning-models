721052256946bdded1f413e01cedbac3c147c67f
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-22 16:37:09 +0600

    Importing the dataset

--- Part-2-Regression/Simple-Linear-Regression/simple_linear_regression.py
+++ Part-2-Regression/Simple-Linear-Regression/simple_linear_regression.py
@@ -4,3 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
