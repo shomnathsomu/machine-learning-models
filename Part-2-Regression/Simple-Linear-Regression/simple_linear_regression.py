0e43ae4a3ad48a7d9b67bcce91467ffbe722874e
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-22 16:39:49 +0600

    Visualising the Training set results

--- Part-2-Regression/Simple-Linear-Regression/simple_linear_regression.py
+++ Part-2-Regression/Simple-Linear-Regression/simple_linear_regression.py
@@ -21,3 +21,11 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
