1e1fc0a7c97fa6b11c8c4459b8e225c448deba5c
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-23 16:59:02 +0600

    Splitting the dataset into Training and Test set

--- Part-2-Regression/Multiple-Linear-Regression/multiple_linear_regression.py
+++ Part-2-Regression/Multiple-Linear-Regression/multiple_linear_regression.py
@@ -20,3 +20,7 @@ X = np.array(ct.fit_transform(X), dtype=np.float)
 
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
