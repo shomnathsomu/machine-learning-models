802e897fc95c373856041bfb76bfd84eacaaac17
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-23 16:57:44 +0600

    Importing the dataset

--- Part-2-Regression/Multiple-Linear-Regression/multiple_linear_regression.py
+++ Part-2-Regression/Multiple-Linear-Regression/multiple_linear_regression.py
@@ -4,3 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
