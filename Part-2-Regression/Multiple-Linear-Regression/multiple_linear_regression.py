3482441d4fc5028d8333ddb63bc569aeb102285e
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-23 16:58:07 +0600

    Encoding categorical data

--- Part-2-Regression/Multiple-Linear-Regression/multiple_linear_regression.py
+++ Part-2-Regression/Multiple-Linear-Regression/multiple_linear_regression.py
@@ -9,3 +9,11 @@ import pandas as pd
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
