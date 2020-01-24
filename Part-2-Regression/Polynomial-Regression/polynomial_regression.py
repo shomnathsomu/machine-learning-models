a4c647e9309dbdbfbb938bdf06e482edeb6169f2
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-24 17:27:31 +0600

    Fitting Linear Regression to the dataset

--- Part-2-Regression/Polynomial-Regression/polynomial_regression.py
+++ Part-2-Regression/Polynomial-Regression/polynomial_regression.py
@@ -9,3 +9,8 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
