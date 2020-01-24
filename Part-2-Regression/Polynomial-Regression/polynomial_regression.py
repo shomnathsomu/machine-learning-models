77aa735fa000fb044d6431207839c7880a905202
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-24 17:26:57 +0600

    Importing the dataset

--- Part-2-Regression/Polynomial-Regression/polynomial_regression.py
+++ Part-2-Regression/Polynomial-Regression/polynomial_regression.py
@@ -4,3 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
