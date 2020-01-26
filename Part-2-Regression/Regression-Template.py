f1da47aff74046822454155aa718dd5378b4be19
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-26 18:32:05 +0600

    Splitting dataset into train and test set

--- Part-2-Regression/Regression-Template.py
+++ Part-2-Regression/Regression-Template.py
@@ -10,3 +10,6 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
