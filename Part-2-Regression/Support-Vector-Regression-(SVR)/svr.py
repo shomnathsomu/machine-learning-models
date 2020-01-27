2527c0654b41ba5fcff3291232b81e21758ed279
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-27 18:04:22 +0600

    Visualising the SVR results
     (for higher resolution and smoother curve)

--- Part-2-Regression/Support-Vector-Regression-(SVR)/svr.py
+++ Part-2-Regression/Support-Vector-Regression-(SVR)/svr.py
@@ -36,3 +36,13 @@ plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
