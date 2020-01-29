f5e890b660048829e383e7adf9d9f00532241c54
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-29 08:27:42 +0600

    Visualising the Random Forest Regression results
    (higher resolution)

--- Part-2-Regression/Random-Forest-Regression/random_forest_regression.py
+++ Part-2-Regression/Random-Forest-Regression/random_forest_regression.py
@@ -29,3 +29,13 @@ regressor.fit(X, y)
 
 # Predicting a new result
 y_pred = regressor.predict([[6.5]])
+
+# Visualising the Random Forest Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
