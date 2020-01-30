491461751eaaa98865d9b514ba80a42138cf1c49
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-30 16:08:26 +0600

    Fitting Logistic Regression to the Training set

--- Part-3-Classification/Logistic-Regression/logistic_regression.py
+++ Part-3-Classification/Logistic-Regression/logistic_regression.py
@@ -19,3 +19,11 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
