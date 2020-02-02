9e1fac9df73df6ca6045b6880ac498ba988c83bc
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-02-03 00:04:35 +0600

    Fitting KNn to the training set

--- Part-3-Classification/K-Nearest-Neighbors-(K-NN)/knn.py
+++ Part-3-Classification/K-Nearest-Neighbors-(K-NN)/knn.py
@@ -19,3 +19,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting K-NN to the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
