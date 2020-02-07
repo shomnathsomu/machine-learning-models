1bb3b2b10035499b7e7c6ccb05f926fa2de16fcb
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-02-08 01:23:14 +0600

    Fitting SVM to the training set

--- Part-3-Classification/Support-Vector-Machine-(SVM)/svm.py
+++ Part-3-Classification/Support-Vector-Machine-(SVM)/svm.py
@@ -19,3 +19,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
