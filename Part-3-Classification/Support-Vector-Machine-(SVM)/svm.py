ac4503cf3e7a475517d268c38d5aa89e2378ecd6
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-02-08 01:21:08 +0600

    Splitting the dataset into training and test set

--- Part-3-Classification/Support-Vector-Machine-(SVM)/svm.py
+++ Part-3-Classification/Support-Vector-Machine-(SVM)/svm.py
@@ -9,3 +9,7 @@ import pandas as pd
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
