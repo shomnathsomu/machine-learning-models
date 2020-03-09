21fa0d150568510491dc2507a811d780500e2699
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-09 10:01:52 +0600

    Predicting the Test set results

--- Part-3-Classification/Naive-Bayes/naive_bayes.py
+++ Part-3-Classification/Naive-Bayes/naive_bayes.py
@@ -24,3 +24,6 @@ X_test = sc.transform(X_test)
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
