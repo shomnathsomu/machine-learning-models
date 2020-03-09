f5a0911ed526c5cda5ef4e2f1b0735a9aea37414
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-09 10:01:30 +0600

    Fitting Naive Bayes to the Training set

--- Part-3-Classification/Naive-Bayes/naive_bayes.py
+++ Part-3-Classification/Naive-Bayes/naive_bayes.py
@@ -19,3 +19,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
