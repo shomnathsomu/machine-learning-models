57c24b61d8e1afddbe6422b4acd248bd47870f80
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-10 09:26:18 +0600

    Fitting into training set, making confusion matrix

--- Part-3-Classification/Random-Forest-Classification/random_forest_classification.py
+++ Part-3-Classification/Random-Forest-Classification/random_forest_classification.py
@@ -19,3 +19,15 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
