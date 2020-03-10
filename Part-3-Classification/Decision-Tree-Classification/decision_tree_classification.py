38ddb8761b9ddc04ac671126d2164714ce9d99ca
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-10 08:52:31 +0600

    Fitting into training set, making confusion matrix

--- Part-3-Classification/Decision-Tree-Classification/decision_tree_classification.py
+++ Part-3-Classification/Decision-Tree-Classification/decision_tree_classification.py
@@ -19,3 +19,15 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting Decision Tree Classification to the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
