07320f9ec24c01441a4349dd364e052b5e04a16b
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-11 11:50:57 +0600

    Importing the Dataset

--- Part-4-Clustering/Hierarchical-Clustering/hc.py
+++ Part-4-Clustering/Hierarchical-Clustering/hc.py
@@ -4,3 +4,8 @@
 # import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+# y = dataset.iloc[:, 3].values
