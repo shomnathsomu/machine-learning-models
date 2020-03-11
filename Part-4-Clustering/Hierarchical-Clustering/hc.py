9626410dba67fa7113a3906f69f8d553cfd5bd7e
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-11 11:52:51 +0600

    Using dendrogram to find optimal clusters number

--- Part-4-Clustering/Hierarchical-Clustering/hc.py
+++ Part-4-Clustering/Hierarchical-Clustering/hc.py
@@ -9,3 +9,11 @@ import pandas as pd
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
