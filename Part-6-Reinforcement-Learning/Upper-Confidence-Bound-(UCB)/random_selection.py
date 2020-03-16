5a7db6ee47e71f80a49d729e2e683711e38dd772
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-16 08:54:17 +0600

    Import the libraries and the dataset

---
+++ Part-6-Reinforcement-Learning/Upper-Confidence-Bound-(UCB)/random_selection.py
@@ -0,0 +1,9 @@
+# Random Selection
+
+# Importing the libraries
+# import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
