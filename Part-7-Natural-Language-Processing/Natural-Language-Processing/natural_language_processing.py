15486d8288b02456f486c1123a1ac82e46bff620
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-18 16:46:07 +0600

    Fitting Naive Bayes to the Training set

--- Part-7-Natural-Language-Processing/Natural-Language-Processing/natural_language_processing.py
+++ Part-7-Natural-Language-Processing/Natural-Language-Processing/natural_language_processing.py
@@ -33,3 +33,8 @@ y = dataset.iloc[:, 1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
