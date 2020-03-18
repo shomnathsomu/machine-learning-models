e0352f49899f343a4bc546c2a126dc4ba662554b
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-18 16:47:21 +0600

    Predict test set results and make confusion matrix

--- Part-7-Natural-Language-Processing/Natural-Language-Processing/natural_language_processing.py
+++ Part-7-Natural-Language-Processing/Natural-Language-Processing/natural_language_processing.py
@@ -38,3 +38,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, rand
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
