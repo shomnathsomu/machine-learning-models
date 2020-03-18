459bf77b1f9217847c02341068e64bde165ba716
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-18 16:45:42 +0600

    Splitting the data set into Training and Test set

--- Part-7-Natural-Language-Processing/Natural-Language-Processing/natural_language_processing.py
+++ Part-7-Natural-Language-Processing/Natural-Language-Processing/natural_language_processing.py
@@ -29,3 +29,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
