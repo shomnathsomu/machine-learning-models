1ac33c56f9dfb76bc8ab54071faf93938efbe0d5
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-31 17:13:49 +0600

    Encoding categorical data

--- Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
+++ Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
@@ -9,5 +9,12 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-# X = dataset.iloc[:, 3:13].values
-# y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+dataset['Geography'] = labelencoder_X_1.fit_transform(dataset['Geography'])
+labelencoder_X_2 = LabelEncoder()
+dataset['Gender'] = labelencoder_X_2.fit_transform(dataset['Gender'])
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
