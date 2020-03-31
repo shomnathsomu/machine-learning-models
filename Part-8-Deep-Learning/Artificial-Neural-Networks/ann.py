e2b1832b368777536d8ec514d708ade72f71a6f3
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-31 17:14:52 +0600

    Feature Scaling

--- Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
+++ Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
@@ -22,3 +22,9 @@ y = dataset.iloc[:, 13].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
