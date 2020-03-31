30a1a41fe2fba857583d6b2c89db9ceafae6e53c
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-31 17:14:30 +0600

    Splitting the dataset into Training and Test set

--- Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
+++ Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
@@ -18,3 +18,7 @@ labelencoder_X_2 = LabelEncoder()
 dataset['Gender'] = labelencoder_X_2.fit_transform(dataset['Gender'])
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
