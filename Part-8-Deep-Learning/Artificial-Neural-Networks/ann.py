e541163762022b5c0ff1c4c03635aea662d90db8
shomnath pramanik <shomnathcse22@gmail.com>
2020-03-31 17:10:40 +0600

    Initiate and import libraries and dataset

---
+++ Part-8-Deep-Learning/Artificial-Neural-Networks/ann.py
@@ -0,0 +1,13 @@
+# Artificial Neural Network
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+# import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+# X = dataset.iloc[:, 3:13].values
+# y = dataset.iloc[:, 13].values
