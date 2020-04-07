6d10bfb634ff432f91af9bdcb1fbdadd980f5a8d
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-07 16:50:32 +0600

    start building the CNN

---
+++ Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
@@ -0,0 +1,11 @@
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
