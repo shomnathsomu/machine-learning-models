b0c1ce26abb3b3dd3b44ca534fcfeeea784d94ee
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-08 14:42:41 +0600

    Initializing the CNN

--- Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
+++ Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
@@ -9,3 +9,6 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
