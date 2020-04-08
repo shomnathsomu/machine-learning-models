113550a808fa9c623ad859e565f1ec260d2c8c78
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-08 14:46:53 +0600

    Full connection

--- Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
+++ Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
@@ -25,3 +25,7 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
