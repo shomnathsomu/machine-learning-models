455d7ad86ec77a230a7cfaa2f93b7f222e20b0e5
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-08 14:43:53 +0600

    Convolution

--- Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
+++ Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
@@ -12,3 +12,6 @@ from keras.layers import Dense
 
 # Initialising the CNN
 classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
