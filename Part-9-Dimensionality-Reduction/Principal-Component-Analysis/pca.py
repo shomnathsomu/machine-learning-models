b31dbf9ed6cc97cde245acc766b8b2ca9a8a91ff
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-09 17:17:59 +0600

    Splitting the dataset into Training and Test set

--- Part-9-Dimensionality-Reduction/Principal-Component-Analysis/pca.py
+++ Part-9-Dimensionality-Reduction/Principal-Component-Analysis/pca.py
@@ -9,3 +9,7 @@ import pandas as pd
 dataset = pd.read_csv('Wine.csv')
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
