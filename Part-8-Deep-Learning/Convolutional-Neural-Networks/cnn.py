53f906a4251512fb796509a4d5cf3bf14642db73
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-09 17:14:52 +0600

    fit the classifier into training_set

--- Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
+++ Part-8-Deep-Learning/Convolutional-Neural-Networks/cnn.py
@@ -53,3 +53,9 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
