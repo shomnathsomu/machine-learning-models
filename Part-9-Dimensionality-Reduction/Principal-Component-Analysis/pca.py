dd184fe0d405c4fde5f59b27ebe08d752634b202
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-09 17:17:16 +0600

    Importing the dataset

--- Part-9-Dimensionality-Reduction/Principal-Component-Analysis/pca.py
+++ Part-9-Dimensionality-Reduction/Principal-Component-Analysis/pca.py
@@ -4,3 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Wine.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
