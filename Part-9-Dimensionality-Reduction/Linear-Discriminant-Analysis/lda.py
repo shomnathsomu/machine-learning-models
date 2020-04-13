eaf53c92899cee1b49f7f8b7a3fb0b4323e688b0
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-13 15:24:19 +0600

    Applying LDA

--- Part-9-Dimensionality-Reduction/Linear-Discriminant-Analysis/lda.py
+++ Part-9-Dimensionality-Reduction/Linear-Discriminant-Analysis/lda.py
@@ -19,3 +19,9 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Applying LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components = 2)
+X_train = lda.fit_transform(X_train, y_train)
+X_test = lda.transform(X_test)
