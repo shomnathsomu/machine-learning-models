c82359850461714b8ad9f6d0f02896e52b61a7f1
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-24 12:37:54 +0600

    Applying Kernel PCA

--- Part-9-Dimensionality-Reduction/Kernel-PCA/kernel_pca.py
+++ Part-9-Dimensionality-Reduction/Kernel-PCA/kernel_pca.py
@@ -19,3 +19,9 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Applying Kernel PCA
+from sklearn.decomposition import KernelPCA
+kpca = KernelPCA(n_components = 2, kernel = 'rbf')
+X_train = kpca.fit_transform(X_train)
+X_test = kpca.transform(X_test)
