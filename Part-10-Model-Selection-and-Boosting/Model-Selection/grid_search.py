f88c11690150c4fa21cec0b76c35d8d1153e7bfe
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-29 11:04:01 +0600

    Importing the dataset

--- Part-10-Model-Selection-and-Boosting/Model-Selection/grid_search.py
+++ Part-10-Model-Selection-and-Boosting/Model-Selection/grid_search.py
@@ -4,3 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
