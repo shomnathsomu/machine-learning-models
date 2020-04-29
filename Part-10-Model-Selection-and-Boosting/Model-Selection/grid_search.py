9fdd37e5e8167f064d17837f290b749f4a034c65
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-29 11:06:44 +0600

    Applying Grid Search to find the best model

--- Part-10-Model-Selection-and-Boosting/Model-Selection/grid_search.py
+++ Part-10-Model-Selection-and-Boosting/Model-Selection/grid_search.py
@@ -37,3 +37,16 @@ from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 accuracies.mean()
 accuracies.std()
+
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+              {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10,
+                           n_jobs = -1)
+grid_search = grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
