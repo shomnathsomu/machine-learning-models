3bb271315cfc665199964e084d294a252d8e73b1
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-29 11:06:07 +0600

    Applying k-Fold Cross Validation

--- Part-10-Model-Selection-and-Boosting/Model-Selection/grid_search.py
+++ Part-10-Model-Selection-and-Boosting/Model-Selection/grid_search.py
@@ -31,3 +31,9 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
