c818229cc12283a7b8ba6afec1cab949585c345a
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-29 11:01:38 +0600

    Initialize and Import the libraries

---
+++ Part-10-Model-Selection-and-Boosting/XGBoost/xgb.py
@@ -0,0 +1,9 @@
+# XGBoost
+
+# Install xgboost following the instructions on this link: 
+# http://xgboost.readthedocs.io/en/latest/build.html
+
+# Importing the libraries
+# import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
