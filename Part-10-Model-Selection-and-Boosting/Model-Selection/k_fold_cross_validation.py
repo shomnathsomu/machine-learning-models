f3c5a9e6d2c973d3f1f47d792e1f40819b094f9c
shomnath pramanik <shomnathcse22@gmail.com>
2020-04-30 10:37:33 +0600

    Fitting Kernel SVM to the Training set

--- Part-10-Model-Selection-and-Boosting/Model-Selection/k_fold_cross_validation.py
+++ Part-10-Model-Selection-and-Boosting/Model-Selection/k_fold_cross_validation.py
@@ -19,3 +19,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
